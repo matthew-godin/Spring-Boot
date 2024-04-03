dd5d672a4dbde676a35fca2e16d703c02010ccf7
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-04-03 17:38:03 -0400

    JSP controller in progress

--- contacts/contacts/src/main/java/com/example/contacts/ContactsApplication.java
+++ contacts/contacts/src/main/java/com/example/contacts/ContactsApplication.java
@@ -2,10 +2,9 @@ package com.example.contacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContactsApplication extends SpringBootServletInitializer {
+public class ContactsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
